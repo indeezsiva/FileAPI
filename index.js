f0eb9f8d963c7d65ebd26168be2b7e2b4c24705f
Harry061298 <hariprasad61298@gmail.com>
2025-06-14 19:27:50 +0530

    Worked on user data CRUD operations

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(express.json());
 // Route imports
 const mediaRoutes = require('./media');
 const fileRoutes = require('./file'); // new file
+const userRoutes = require('./UserData'); 
 
 // Register routes
 app.use('/media', mediaRoutes);
 app.use('/file', fileRoutes);
+app.use('/user', userRoutes);
 
 module.exports = app;
